e3594171791f1a07419ab9545ed354ad62d0c60d
Boureima Tapily <boureimatapily1@gmail.com>
2020-01-28 14:03:51 +0000

    Add Components

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,7 +3,20 @@ import React from 'react'
 const Dashboard = () => {
   return (
     <div>
-      
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h3 className="panel-title">Panel title</h3>
+              </div>
+              <div class="panel-body">
+                Panel content
+               </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
